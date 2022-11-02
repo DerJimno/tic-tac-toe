06d464a877ae59b1e7187a9d12676b6796861bfd
Jimno <menaoir.jimmy@gmail.com>
2022-11-02 14:35:24 +0100

    improve readability more

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,29 +1,41 @@
 class TicTacToe
+# Board things 
   attr_reader :board
   WIN_COMBINATIONS = [
-    [0,1,2], [3,4,5], [6,7,8],[0,4,8],
-    [2,4,6], [0,3,6],[1,4,7], [2,5,8]
+    [0,1,2], [3,4,5], [6,7,8], [0,4,8],
+    [2,4,6], [0,3,6], [1,4,7], [2,5,8]
   ]
 
   def initialize
-    @board = Array.new(9, " ")
+    @board = [*1..9]
   end
 
   def display_board
     puts <<-MULTI
-      #{@board[0]}|#{@board[1]}|#{@board[2]}
-      #{@board[3]}|#{@board[4]}|#{@board[5]}
-      #{@board[6]}|#{@board[7]}|#{@board[8]}
+
+      #{board[0]}|#{board[1]}|#{board[2]}
+      #{board[3]}|#{board[4]}|#{board[5]}
+      #{board[6]}|#{board[7]}|#{board[8]}
+
     MULTI
   end
 
   def update_board(input, symbol = "X")
-    @board[input - 1] = symbol
+    board[input - 1] = symbol
   end
 
   def valid_move?(input)
-    @board[input - 1] == " "
+    board[input - 1] == input
   end
+
+  def full_board?
+    board.all? { |element| element == "X" || element == "O"}
+  end
+# Game things
+
 end
 
 game = TicTacToe.new
+
+
+
